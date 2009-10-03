1bf5cfe5ad8b6f8d1ffd905f11228870735b82ee
sdsykes <sdsykes@gmail.com>
2009-10-03 19:46:30 +0300

    Limit log files to 25Mb.

--- lib/background_fu.rb
+++ lib/background_fu.rb
@@ -5,6 +5,6 @@ module BackgroundFu
   CONFIG = File.exist?(CONFIG_FILE) && YAML::load_file(CONFIG_FILE)['background_fu'] || {}
   CONFIG['cleanup_interval'] ||= :on_startup
   CONFIG['monitor_interval'] ||= 10
-  BACKGROUND_LOGGER = Logger.new(File.expand_path(RAILS_ROOT+"/log/background_#{Rails.env}.log"), 2, 5000000)
+  BACKGROUND_LOGGER = Logger.new(File.expand_path(RAILS_ROOT+"/log/background_#{Rails.env}.log"), 2, 25000000)
 
 end
